83e914ed83a2f6ed6b7a80b6efc5ee82cf1e6447
mmzk1526 <yc4120@ic.ac.uk>
2022-12-17 22:10:18 +0000

    [mmzk] (feat) Day17

--- src/Day16.hs
+++ src/Day16.hs
@@ -4,40 +4,42 @@
 
 import           Control.Monad
 import           Control.Monad.Trans.State
-import           Data.Array
 import           Data.Bifunctor
 import           Data.Bits
-import           Data.List
 import           Data.Map (Map)
 import qualified Data.Map as M
 import qualified Data.Set as S
 import           Data.Text (Text)
 import qualified Data.Text as T
-import           Gadgets.Array
 import           Utilities
 
 mkCaves :: [Text] -> Map Int (Int, [(Int, Int)])
 mkCaves strs = M.fromList $ toIndex <$> M.keys compactMap
   where
-    toIndex k  = (indices M.! k, second (map (first (indices M.!))) $ compactMap M.! k)
-    indices    = M.fromList $ zip (fst <$> sortOn (fst . snd) (M.toList compactMap)) [0..]
+    toIndex k  = (getIx k, second (map (first getIx)) $ compactMap M.! k)
+    getIx      = ((M.fromList $ zip (M.keys compactMap) [0..]) M.!)
     compactMap = M.fromList $ concatMap compress (M.keys tempMap)
     compress k
-      | fst (tempMap M.! k) > 0 || k == "AA" = [(k, (fst (tempMap M.! k), go (S.singleton k) . zip (snd (tempMap M.! k)) $ repeat 1))]
-      | otherwise                            = []
+      | fst (tempMap M.! k) > 0 || k == "AA" 
+      = [ (k, (fst (tempMap M.! k)
+        , go (S.singleton k) . zip (snd (tempMap M.! k)) $ repeat 1)) ]
+      | otherwise = []
     go _ []    = []
     go vis ((fr, dist) : frs)
-      | fr `elem` vis             = go vis frs
-      | fst (tempMap M.! fr) /= 0 = (fr, dist) : go (S.insert fr vis) (frs ++ [(fr', dist + 1) | fr' <- snd (tempMap M.! fr)])
-      | otherwise                 = go (S.insert fr vis) (frs ++ [(fr', dist + 1) | fr' <- snd (tempMap M.! fr)])
+      | fr `elem` vis = go vis frs
+      | value /= 0    = (fr, dist) : go (S.insert fr vis) frs'
+      | otherwise     = go (S.insert fr vis) frs'
+      where
+        (value, neighbours) = tempMap M.! fr
+        frs'                = frs ++ [(fr', dist + 1) | fr' <- neighbours]
     tempMap    = M.fromList $ worker <$> strs
     worker str = (name, (value, caves))
       where
-        (this, others) = bimap T.words (breakOn " ") $ breakOn "; " str
-        (name, value)  = (this !! 1, readInt . snd . breakOn "=" $ last this)
-        caves          = T.splitOn ", " . snd $ case fst others of
-          "tunnel" -> breakOn "valve " $ snd others
-          _        -> breakOn "valves " $ snd others
+      (this, others) = bimap T.words (breakOn " ") $ breakOn "; " str
+      (name, value)  = (this !! 1, readInt . snd . breakOn "=" $ last this)
+      caves          = T.splitOn ", " . snd $ case fst others of
+        "tunnel" -> breakOn "valve " $ snd others
+        _        -> breakOn "valves " $ snd others
 
 mkCache :: Int -> Map Int (Int, [(Int, Int)]) -> Map Int Int
 mkCache time caves = execState (buildCache time 0 0 0) M.empty
@@ -49,14 +51,14 @@ mkCache time caves = execState (buildCache time 0 0 0) M.empty
         buildCache (rem - 1 - cost) pos' (setBit c pos) acc'
 
 day16Part1 :: Map Int (Int, [(Int, Int)]) -> Int
-day16Part1 caves = maximum (mkCache 30 caves)
+day16Part1 = maximum . mkCache 30
 
 day16Part2 :: Map Int (Int, [(Int, Int)]) -> Int
 day16Part2 caves
-  = maximum [cache M.! i + cache M.! j | i <- keys, j <- keys, i .&. j == 1]
+  = maximum [ cache M.! i + cache M.! j
+            | (ix, i) <- zip [0..] keys, j <- drop (ix + 1) keys, i .&. j == 1 ]
   where
-    cache = mkCache 26 caves
-    keys  = M.keys cache
+    (cache, keys) = (mkCache 26 caves, M.keys cache)
 
 main :: IO ()
 main = do
---
+++ src/Day17.hs
@@ -0,0 +1,114 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+-- Question source: https://adventofcode.com/2022/day/17
+
+import           Control.Monad
+import           Control.Monad.ST.Lazy
+import           Control.Monad.Trans.Class
+import           Control.Monad.Trans.State
+import           Data.Array.ST
+import           Data.Bifunctor
+import           Data.InfList (InfList(..))
+import qualified Data.InfList as I
+import           Data.IntSet (IntSet)
+import qualified Data.IntSet as IS
+import           Data.Map (Map)
+import qualified Data.Map as M
+import qualified Data.Text as T
+import           Gadgets.Array.Mutable as A
+import qualified Gadgets.Array.ST.Lazy as A
+import           Utilities
+
+data Shape = Shape { bottom :: (Int, Int), offsets :: [(Int, Int)] }
+
+type SimulationState = ( Int, InfList (Int, Int -> Shape), InfList (Int, Bool)
+                       , Map (Int, Int) (Int, Int) )
+
+r1, r2, r3, r4, r5 :: Int -> Shape
+r1 h = Shape (0, h) [(-1, 0), (1, 0), (2, 0)]
+r2 h = Shape (0, h) [(0, 1), (0, 2), (-1, 1), (1, 1)]
+r3 h = Shape (0, h) [(-1, 0), (1, 0), (1, 1), (1, 2)]
+r4 h = Shape (-1, h) [(0, 1), (0, 2), (0, 3)]
+r5 h = Shape (0, h) [(-1, 0), (0, 1), (-1, 1)]
+
+allOffsets :: Shape -> [(Int, Int)]
+allOffsets Shape { bottom = b@(x, y), offsets = ofts }
+  = b : fmap (bimap (x +) (y +)) ofts
+
+shift :: Bool -> Shape -> Shape
+shift True shape@Shape { bottom = (x, y) }  = shape { bottom = (x + 1, y) }
+shift False shape@Shape { bottom = (x, y) } = shape { bottom = (x - 1, y) }
+
+down :: Shape -> Shape
+down shape@Shape { bottom = (x, y) } = shape { bottom = (x, y - 1) }
+
+simulateOne :: Int -> STArray s Int IntSet
+            -> StateT SimulationState (ST s) Int
+simulateOne count boardST = do
+  (h, (ix, shape) ::: shapes, dirs, cache) <- get
+  let cache' = M.insert (ix, fst $ I.head dirs) (count, h) cache
+  (h', dirs')                              <- fall (shape (h + 4)) dirs
+  put (max h h', shapes, dirs', cache')
+  return $ count + 1
+  where
+    isIn (x, y)
+      | x < -3 || x > 3 = pure False
+      | otherwise       = IS.notMember y <$> (boardST ! x)
+    fall shape ((_, dir) ::: dirs) = do
+      let xOfts   = fst <$> allOffsets shape
+      let onSide  = all (inRange (-3, 3) . (if dir then succ else pred)) xOfts
+      moveable <- fmap and . lift $ forM (allOffsets (shift dir shape)) isIn
+      let shape'  = if moveable then shift dir shape else shape
+      let shape'' = down shape'
+      moveable <- fmap and . lift $ forM (allOffsets shape'') isIn
+      if moveable
+        then fall shape'' dirs
+        else do
+          lift . forM_ (allOffsets shape') $ \(x, y) ->
+              adjust' boardST (IS.insert y) x
+          return (snd (bottom shape') + maximum (snd <$> offsets shape'), dirs)
+
+naiveSimulation :: Int -> InfList (Int, Bool) -> Int
+naiveSimulation count dirs = runST $ do
+  boardST <- A.newArray (-3, 3) $ IS.singleton 0
+  (h, _, _, _) <- execStateT (replicateM count $ simulateOne 0 boardST)
+                             ( 0, I.cycle $ zip [1..] [r1, r2, r3, r4, r5]
+                             , dirs, M.empty)
+  return h
+
+simulation :: Int -> InfList (Int, Bool) -> Int
+simulation count dirs = runST $ do
+  boardST <- A.newArray (-3, 3) $ IS.singleton 0
+  ((ni, oi), (nh, oh))    <- takeArith <$> evalStateT (worker 1 boardST) init
+  -- error $ show $ I.take 10 list
+  -- let ((ni, oi), (nh, oh)) = takeArith list
+  if count < oh
+    then pure $ naiveSimulation count dirs
+    else do
+      let (repeatCount, remainder) = quotRem (count - oh) (ni - oi)
+      return $ repeatCount * (nh - oh) + naiveSimulation (oh + remainder) dirs
+  where
+    takeArith ((a1, b1) ::: n2@(a2, b2) ::: n3@(a3, b3) ::: ns)
+      | a3 - a2 == a2 - a1 && b3 - b2 == b2 - b1 = ((a2, a1), (b2, b1))
+      | otherwise                                = takeArith $ n2 ::: n3 ::: ns
+    init = (0, I.cycle $ zip [1..] [r1, r2, r3, r4, r5], dirs, M.empty)
+    worker i boardST = do
+      (h, (sIx, _) ::: _, (dIx, _) ::: _, cache) <- get
+      simulateOne i boardST
+      case cache M.!? (sIx, dIx) of
+        Nothing -> worker (i + 1) boardST
+        Just x  -> do
+          modify (\(h, s, d, _) -> (h, s, d,  M.singleton (sIx, dIx) (i, h)))
+          (x :::) <$> worker (i + 1) boardST
+
+day17Part1 :: InfList (Int, Bool) -> Int
+day17Part1 = naiveSimulation 2022
+
+day17Part2 :: InfList (Int, Bool) -> Int
+day17Part2 = simulation 1000000000000
+
+main :: IO ()
+main = do
+  input <- I.cycle . zip [1..] . map (== '>') . T.unpack <$> readInput "day17"
+  print $ day17Part1 input
+  print $ day17Part2 input
